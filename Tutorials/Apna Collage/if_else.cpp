a60c0c7d0fd481cc3476a402aa672b48ef8161ce
sagario9 <saikatsardar180@gmail.com>
2024-10-09 22:23:08 +0530

    Updated

---
+++ Tutorials/Apna Collage/if_else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+// Checking in abc which is greater
+
+int main(){
+
+    int n;
+    cin >> n;
+    if(n%2==0){
+        cout<<"Even";
+    }else {
+        cout << "ODD";
+    }
+    return 0;
+}
